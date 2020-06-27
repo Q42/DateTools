f3d622525a464c3cbaab47a3f237b9d4c7233ded
Mathijs ‏Bernson <mathijsb@q42.nl>
2020-06-27 16:13:36 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,22 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "DateToolsSwift",
-    targets: [
-        Target(name: "DateToolsSwift")
-    ]
+  name: "DateToolsSwift",
+  products: [
+    .library(
+      name: "DateToolsSwift",
+      targets: ["DateToolsSwift"]
+    ),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "DateToolsSwift",
+      dependencies: [],
+      path: "DateToolsSwift/DateTools/",
+      exclude: ["DateTools.bundle"]
+    ),
+  ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
